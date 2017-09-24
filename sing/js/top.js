c4f3b8683f9e0da36e0d1b841ef9d945bda2ff4f
tuanvampire <gmo.dinhtuan@gmail.com>
2017-09-24 07:54:19 +0700

    update

---
+++ sing/js/top.js
@@ -0,0 +1,14 @@
+	$(function() {
+	$(window).scroll(function() {
+	if ($(this).scrollTop() != 0) {
+	$('#bttop').fadeIn();
+	} else {
+	$('#bttop').fadeOut();
+	}
+	});
+	$('#bttop').click(function() {
+	$('body,html').animate({
+	scrollTop: 0
+	}, 800);
+	});
+	});
